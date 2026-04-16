5ded310fd5e0d39ef14c0f92c4f59327bf53c60b
wambuimuna <wambomuna@gmail.com>
2026-04-16 16:42:43 +0300

    Analyzing Data Modules

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+};
 
+for (const arg of args) {
+  combinedObject.users = [...combinedObject.users, ...arg];
+}
 
+combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+ return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
+
 };
